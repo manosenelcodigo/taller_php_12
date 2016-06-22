d02231080d30b98ffe76764b54f1012ee04545d2
#manosenelcódigo <peligrosamente@gmail.com>
2016-06-21 23:07:26 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Creación y lectura de reporte de un XML</title>
+    </head>
+    <body>
+        <h1>Creación y lectura de reporte de un XML</h1>
+        <ul>
+            <li> <a href="crear.php">Crear</a> </li>
+            <li><a href="leer.php">Leer</a> </li>
+        </ul>
+    </body>
+    </html>
